36ab4b7b5b1dc566c85dd6774d9d11af44d8d94c
Heindrick <hcheung01@gmail.com>
2017-09-18 15:36:24 -0700

    2nd copy

--- roulettefinal.py
+++ roulettefinal.py
@@ -9,29 +9,23 @@ bank_account = 1000
 bet_amount = int(input("Place bet amount please"))
 bet_color = input("What color do you want to bet on?")
 bet_number = int(input("What numbers do you want to bet on?"))
+print("-----------------------------------")
+print("-----------------------------------")
 
-def take_bet(color, number, amount):
-  bet_color = color
-  bet_number = number
-  bet_amount = amount
+roll_ballnumber = random.randint(0, 37)
+print("Landing number is: ")
+print(roll_ballnumber)
 
-def roll_ball():
-  roll_ballnumber = random.randint(0, 37)
-  print("Winning number is:")
-  return roll_ballnumber
-print(roll_ball())
+if bet_number == roll_ballnumber:
+  print("You Win")
+else:
+  print("You Lose")
+print("-----------------------------------")
+print("Landing color is: ")
 
-def check_results():
-  if bet_number in red:
-    print("red")
-  elif bet_number in black:
-    print("black")
-  elif bet_number in green:
-    print("green")
-
-  if roll_ballnumber in red:
-    print("Red")
-  elif roll_ballnumber in black:
-    print("Black")
-  else:
-    print("Green")
+if roll_ballnumber in red:
+  print("Red Wins")
+elif roll_ballnumber in black:
+  print("Black Wins")
+else:
+  print("Green Wins")
